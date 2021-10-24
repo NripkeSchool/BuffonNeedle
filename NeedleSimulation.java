81f953f40d16bc893db8850f03d7d191ade019e1
NripkeSchool <89931379+NripkeSchool@users.noreply.github.com>
2021-10-24 18:44:35 -0400

    Finished NeedleSimulation

--- NeedleSimulation.java
+++ NeedleSimulation.java
@@ -4,7 +4,7 @@ public class NeedleSimulation
 {
    public static void main(String[] args)
    {
-       Tarp myTarp = new Tarp(10, 20); //Length of needle is 10, length between lines is 10
+       Tarp myTarp = new Tarp(10, 10); //Length of needle is 10, length between lines is 10
        Scanner scan = new Scanner(System.in);
        
        System.out.println("Insert amount of iterations: ");
@@ -12,7 +12,7 @@ public class NeedleSimulation
        
        myTarp.addRandomNeedle(iterations);
        System.out.println("Probability: " + myTarp.fractionOfCrossings());
-       double pi;
-       System.out.println("Approximation of PI: ");
+       double pi = (1/myTarp.fractionOfCrossings())*2;
+       System.out.println("Approximation of PI: " + pi);
    }
 }
